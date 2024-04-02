d0d5b1860065665b2c2b23570582c66aef3d842b
Karthik Kalyanaraman <105607645+karthikscale3@users.noreply.github.com>
2024-04-02 16:39:21 -0700

    Fix (#30)

--- app/(protected)/layout.tsx
+++ app/(protected)/layout.tsx
@@ -1,7 +1,6 @@
 import { Header } from "@/components/shared/header";
 import { Separator } from "@/components/ui/separator";
 import { authOptions } from "@/lib/auth/options";
-import prisma from "@/lib/prisma";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import { Suspense } from "react";
@@ -16,25 +15,10 @@ export default async function Layout({
     redirect("/login");
   }
 
-  const user = await prisma.user.findUnique({
-    where: {
-      email: session?.user?.email as string,
-    },
-  });
-
-  // if user is not found, redirect to login
-  if (!user) {
-    redirect("/login");
-  }
-
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <main className="min-h-screen w-full">
-        <Header
-          name={user.name as string}
-          email={user.email as string}
-          avatar={user.image as string}
-        />
+        <Header email={session?.user?.email as string} />
         <Separator />
         {children}
       </main>
