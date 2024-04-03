189f35a373ac15b84c4fd495d58a1f3b1341c6aa
Karthik Kalyanaraman <105607645+karthikscale3@users.noreply.github.com>
2024-04-03 13:40:17 -0700

    Release (#35)
    * Bug fix
    
    * signup bug fix
    
    * user invite
    
    * docs: add local setup instructions, cleanup files (#10)
    
    * Update README.md (#11)
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Bugfix auth (#12)
    
    * Update README.md
    
    * Update favicon (#17)
    
    * Release 1.0.3 (#8)
    
    * Bug fix
    
    * signup bug fix
    
    * user invite
    
    * favicon
    
    * Adding pagination to traces (#16)
    
    * Adding pagination to traces
    
    * installing scroller
    
    * query function bug
    
    * fix
    
    * Bug fix pagination
    
    ---------
    
    Co-authored-by: Karthik Kalyanaraman <karthik@scale3labs.com>
    
    * Dylan/s3en 2060 adding more frontend pagination (#18)
    
    * Adding pagination to traces
    
    * installing scroller
    
    * bug fixes for trace pagination
    
    * adding delete prompt api
    
    * adding pagination to evaluate
    
    * adding pagination to evals
    
    * adding pagination to prompset, bug fixes
    
    * adding .env
    
    * fix
    
    ---------
    
    Co-authored-by: Karthik Kalyanaraman <karthik@scale3labs.com>
    
    * updating prompt dialog (#19)
    
    * Show API key warning message (#20)
    
    * Show API key warning message
    
    * Update instructions
    
    * Fix null dereference checks (#21)
    
    * adding check for missing table (#22)
    
    * adding check for missing table
    
    * adding docs button
    
    * Check empty table
    
    ---------
    
    Co-authored-by: Karthik Kalyanaraman <karthik@scale3labs.com>
    
    * Check length of data (#24)
    
    * flicker issue (#26)
    
    * Flicker issue (#27)
    
    * flicker issue
    
    * Fixes
    
    * Fix (#30)
    
    * Fix header (#32)
    
    * updating gitignore
    
    * Rohit/s3 en 2064 fix api key not showing (#29)
    
    * fix api key bug
    
    * final fixes
    
    * fix scroll bar issue
    
    ---------
    
    Co-authored-by: darshit-s3 <119623510+darshit-s3@users.noreply.github.com>
    Co-authored-by: Rohit Kadhe <113367036+rohit-kadhe@users.noreply.github.com>
    Co-authored-by: dylanzuber-scale3 <116033320+dylanzuber-scale3@users.noreply.github.com>
    Co-authored-by: dylan <dylan@scale3labs.com>

--- components/shared/setup-instructions.tsx
+++ components/shared/setup-instructions.tsx
@@ -9,7 +9,11 @@ export default function SetupInstructions({
   project_id: string;
 }) {
   const [sdk, setSdk] = useState("typescript");
+  const [apiKey, setApiKey] = useState('<LANGTRACE_API_KEY>');
 
+  const handleApiKeyGenerated = (newApiKey: string) => {
+    setApiKey(newApiKey);
+  };
   const copyToClipboard = (code: string) => {
     navigator.clipboard.writeText(code);
     return toast.success("Copied to clipboard");
@@ -55,24 +59,24 @@ export default function SetupInstructions({
         <p className="text-sm text-muted-foreground">
           2. Generate and copy your API key.
         </p>
-        <GenerateApiKey projectId={project_id} />
+        <GenerateApiKey projectId={project_id} onApiKeyGenerated={handleApiKeyGenerated}/>
       </div>
       <div className="flex flex-col gap-3">
         <p className="text-sm text-muted-foreground">
-          3. Initialize the SDK with the following code snippet. Set the env
-          var, LANGTRACE_API_KEY to the API key you generated.
+          3. Initialize the SDK with the following code snippet
         </p>
         {sdk === "typescript" && (
           <pre
             className="text-xs p-2 rounded-md bg-muted select-all selection:bg-orange-400 dark:selection:bg-orange-600"
             onClick={() => {
               copyToClipboard(
-                "import { init } from '@langtrace-init/init';\n\ninit({ api_key: <api-key-from-step-2> });"
+                `//Must precede any llm module imports\n\nimport * as Langtrace from '@langtrase/typescript-sdk'\n\nLangtrace.init({ api_key: '${apiKey ?? '<LANGTRACE_API_KEY>'}' })`
               );
             }}
           >
-            {"import { init } from '@langtrace-init/init';\n\n"}
-            {"init({ api_key: <api-key-from-step-2> });"}
+            {"// Must precede any llm module imports\n\n"}
+            {"import * as Langtrace from '@langtrase/typescript-sdk'\n\n"}
+            {`Langtrace.init({ api_key: '${apiKey ?? '<LANGTRACE_API_KEY>'}' })`}
           </pre>
         )}
         {sdk === "python" && (
@@ -80,12 +84,13 @@ export default function SetupInstructions({
             className="text-xs p-2 rounded-md bg-muted select-all selection:bg-orange-400 dark:selection:bg-orange-600"
             onClick={() => {
               copyToClipboard(
-                "from langtrace_python_sdk import langtrace\n\nlangtrace.init(api_key=<api-key-from-step-2>)"
+                `# Must precede any llm module imports\n\nfrom langtrace_python_sdk import langtrace\n\nlangtrace.init({ api_key = '${apiKey ?? '<LANGTRACE_API_KEY>'}')`
               );
             }}
           >
+            {"# Must precede any llm module imports\n\n"}
             {"from langtrace_python_sdk import langtrace\n\n"}
-            {"langtrace.init(api_key=<api-key-from-step-2>)"}
+            {`langtrace.init({ api_key = '${apiKey ?? '<LANGTRACE_API_KEY>'}')`}
           </pre>
         )}
       </div>
