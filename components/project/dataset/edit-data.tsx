bc149cd61d85f2f2d9ec9cdab119bf0257f99a67
dylan <dylan@scale3labs.com>
2024-04-04 11:43:25 -0400

    removing form reset on edit

--- components/project/dataset/edit-data.tsx
+++ components/project/dataset/edit-data.tsx
@@ -137,7 +137,6 @@ export function EditData({
                     description: "Your data has been saved.",
                   });
                   setOpen(false);
-                  EditDataForm.reset();
                 } catch (error: any) {
                   toast("Error saving your dataset!", {
                     description: `There was an error saving your dataset: ${error.message}`,
