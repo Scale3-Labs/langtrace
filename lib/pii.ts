d1d54b85c72b419d08f74fffa010fcec88e54095
Karthik Kalyanaraman <105607645+karthikscale3@users.noreply.github.com>
2024-04-04 23:43:51 -0700

    Release (#46)
    * Bug fix
    
    * signup bug fix
    
    * user invite
    
    * docs: add local setup instructions, cleanup files (#10)
    
    * Update README.md (#11)
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Bugfix auth (#12)
    
    * Update README.md
    
    * Update favicon (#17)
    
    * Release 1.0.3 (#8)
    
    * Bug fix
    
    * signup bug fix
    
    * user invite
    
    * favicon
    
    * Adding pagination to traces (#16)
    
    * Adding pagination to traces
    
    * installing scroller
    
    * query function bug
    
    * fix
    
    * Bug fix pagination
    
    ---------
    
    Co-authored-by: Karthik Kalyanaraman <karthik@scale3labs.com>
    
    * Dylan/s3en 2060 adding more frontend pagination (#18)
    
    * Adding pagination to traces
    
    * installing scroller
    
    * bug fixes for trace pagination
    
    * adding delete prompt api
    
    * adding pagination to evaluate
    
    * adding pagination to evals
    
    * adding pagination to prompset, bug fixes
    
    * adding .env
    
    * fix
    
    ---------
    
    Co-authored-by: Karthik Kalyanaraman <karthik@scale3labs.com>
    
    * updating prompt dialog (#19)
    
    * Show API key warning message (#20)
    
    * Show API key warning message
    
    * Update instructions
    
    * Fix null dereference checks (#21)
    
    * adding check for missing table (#22)
    
    * adding check for missing table
    
    * adding docs button
    
    * Check empty table
    
    ---------
    
    Co-authored-by: Karthik Kalyanaraman <karthik@scale3labs.com>
    
    * Check length of data (#24)
    
    * flicker issue (#26)
    
    * Flicker issue (#27)
    
    * flicker issue
    
    * Fixes
    
    * Fix (#30)
    
    * Fix header (#32)
    
    * updating gitignore
    
    * Rohit/s3 en 2064 fix api key not showing (#29)
    
    * fix api key bug
    
    * final fixes
    
    * fix scroll bar issue
    
    * adjust readme
    
    * Pagination bug
    
    * Pagination bug (#37)
    
    * removing form reset on edit (#39)
    
    * removing form reset on edit
    
    * bug fixes:
    
    * fix
    
    * fix page number bug
    
    ---------
    
    Co-authored-by: Karthik Kalyanaraman <karthik@scale3labs.com>
    
    * Dylan/s3en 2079 thumbs up duplication bug (#40)
    
    * refetch/duplicate bug fix
    
    * Prevent stale data in forms
    
    * fix
    
    * linting
    
    * fixing duplication bug
    
    * removing console
    
    * Fix eval
    
    * Fix pagination for promp eval
    
    * Fix promptsets pagination
    
    ---------
    
    Co-authored-by: Karthik Kalyanaraman <karthik@scale3labs.com>
    
    * Bug fixes for Traces Pagination (#42)
    
    * Pagination bug
    
    * Fix pagination for traces
    
    * Bug fix
    
    * Fix invalid model (#43)
    
    * Pagination bug
    
    * Bug fix
    
    * Bug fixes (#45)
    
    * Pagination bug
    
    * Bug fix
    
    * fix pricing
    
    * fix the pii function
    
    * Bugfixes
    
    ---------
    
    Co-authored-by: darshit-s3 <119623510+darshit-s3@users.noreply.github.com>
    Co-authored-by: Rohit Kadhe <113367036+rohit-kadhe@users.noreply.github.com>
    Co-authored-by: dylanzuber-scale3 <116033320+dylanzuber-scale3@users.noreply.github.com>
    Co-authored-by: dylan <dylan@scale3labs.com>
    Co-authored-by: Ali Waleed <134522290+alizenhom@users.noreply.github.com>

--- lib/pii.ts
+++ lib/pii.ts
@@ -1,25 +1,30 @@
 export default function detectPII(text: string): string[] {
-  // Define regular expressions for various types of PII
-  const patterns = {
-    phoneNumber: /\b\d{3}[-.]?\d{3}[-.]?\d{4}\b/g, // Simplified phone number pattern (US-centric)
-    email: /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b/g, // Email pattern
-    ssn: /\b\d{3}-\d{2}-\d{4}\b/g, // SSN pattern
-    dateOfBirth:
-      /\b(0?[1-9]|1[012])[- /.](0?[1-9]|[12][0-9]|3[01])[- /.](19|20)?\d\d\b/g, // Date of birth pattern MM/DD/YYYY, MM-DD-YYYY, etc.
-    creditCard: /\b(?:\d{4}[- ]?){3}\d{4}\b/g, // Basic credit card pattern
-    ipAddress: /\b(?:\d{1,3}\.){3}\d{1,3}\b/g, // Simplistic IP address pattern
-    // Add more patterns as needed
-  };
+  try {
+    JSON.parse(text);
+    return [];
+  } catch (e) {
+    // Define regular expressions for various types of PII
+    const patterns = {
+      phoneNumber: /\b\d{3}[-.]?\d{3}[-.]?\d{4}\b/g, // Simplified phone number pattern (US-centric)
+      email: /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}\b/g, // Email pattern
+      ssn: /\b\d{3}-\d{2}-\d{4}\b/g, // SSN pattern
+      dateOfBirth:
+        /\b(0?[1-9]|1[012])[- /.](0?[1-9]|[12][0-9]|3[01])[- /.](19|20)?\d\d\b/g, // Date of birth pattern MM/DD/YYYY, MM-DD-YYYY, etc.
+      creditCard: /\b(?:\d{4}[- ]?){3}\d{4}\b/g, // Basic credit card pattern
+      ipAddress: /\b(?:\d{1,3}\.){3}\d{1,3}\b/g, // Simplistic IP address pattern
+      // Add more patterns as needed
+    };
 
-  const foundPII: string[] = [];
+    const foundPII: string[] = [];
 
-  // Check the text against each pattern and add matches to foundPII
-  for (const [type, pattern] of Object.entries(patterns)) {
-    const matches = text.match(pattern);
-    if (matches) {
-      foundPII.push(...matches.map((match) => `${type}: ${match}`));
+    // Check the text against each pattern and add matches to foundPII
+    for (const [type, pattern] of Object.entries(patterns)) {
+      const matches = text.match(pattern);
+      if (matches) {
+        foundPII.push(...matches.map((match) => `${type}: ${match}`));
+      }
     }
-  }
 
-  return foundPII;
+    return foundPII;
+  }
 }
