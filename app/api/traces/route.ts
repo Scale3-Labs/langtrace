a1025f71182c8d20bdc7696750dba89d029e366c
dylan <dylan@scale3labs.com>
2024-05-01 12:46:46 -0400

    adding api access to traces endpoint

--- app/api/traces/route.ts
+++ app/api/traces/route.ts
@@ -1,4 +1,5 @@
 import { authOptions } from "@/lib/auth/options";
+import prisma from "@/lib/prisma";
 import { TraceService } from "@/lib/services/trace_service";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
@@ -8,11 +9,33 @@ export async function POST(req: NextRequest) {
   try {
     const session = await getServerSession(authOptions);
 
-    if (!session || !session.user) {
-      redirect("/login");
+    const apiKey = req.headers.get("x-api-key");
+
+    if (!apiKey) {
+      if (!session || !session.user) {
+        redirect("/login");
+      }
     }
 
     const { page, pageSize, projectId, filters } = await req.json();
+
+    const project = await prisma.project.findFirst({
+      where: {
+        id: projectId,
+      },
+    });
+
+    if (!project) {
+      return Response.json({ error: "No projects found" }, { status: 404 });
+    }
+
+    if (apiKey && project.apiKeyHash !== hashApiKey(apiKey)) {
+      return Response.json(
+        { error: "Unauthorized. Invalid API key" },
+        { status: 401 }
+      );
+    }
+
     const traceService = new TraceService();
     const traces = await traceService.GetTracesInProjectPaginated(
       projectId,
