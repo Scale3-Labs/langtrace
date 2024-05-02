1426245338c2e548df5c473803b1913b3f3175fb
dylan <dylan@scale3labs.com>
2024-05-02 12:49:41 -0400

    refactor

--- app/api/traces/route.ts
+++ app/api/traces/route.ts
@@ -9,32 +9,32 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(req: NextRequest) {
   try {
     const session = await getServerSession(authOptions);
-
     const apiKey = req.headers.get("x-api-key");
+    const { page, pageSize, projectId, filters } = await req.json();
+    if (!session || !session.user) {
+      if (apiKey) {
+        const project = await prisma.project.findFirst({
+          where: {
+            id: projectId,
+          },
+        });
 
-    if (!apiKey) {
-      if (!session || !session.user) {
+        if (!project) {
+          return NextResponse.json(
+            { error: "No projects found" },
+            { status: 404 }
+          );
+        }
+
+        if (apiKey && project.apiKeyHash !== hashApiKey(apiKey)) {
+          return NextResponse.json(
+            { error: "Unauthorized. Invalid API key" },
+            { status: 401 }
+          );
+        }
+      } else {
         redirect("/login");
       }
-    }
-
-    const { page, pageSize, projectId, filters } = await req.json();
-
-    const project = await prisma.project.findFirst({
-      where: {
-        id: projectId,
-      },
-    });
-
-    if (!project) {
-      return NextResponse.json({ error: "No projects found" }, { status: 404 });
-    }
-
-    if (apiKey && project.apiKeyHash !== hashApiKey(apiKey)) {
-      return NextResponse.json(
-        { error: "Unauthorized. Invalid API key" },
-        { status: 401 }
-      );
     }
 
     const traceService = new TraceService();
