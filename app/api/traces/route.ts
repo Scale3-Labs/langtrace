c46453c3070e5a4e1541efcbf4d6387e58ae7ef5
darshit-s3 <119623510+darshit-s3@users.noreply.github.com>
2024-05-06 09:36:19 +0530

    Release  (#80)
    * chore: add docker cmd
    
    * Compatibility fixes for SDK version 2.0.0 (#69)
    
    * Pagination bug
    
    * Bug fix
    
    * Fix for schema changes
    
    * Render tool calling
    
    * Support for Langgraph, Qdrant & Groq  (#73)
    
    * Pagination bug
    
    * Bug fix
    
    * Add langgraph support
    
    * QDrant support
    
    * Add Groq support
    
    * update README
    
    * update README
    
    * feat: optimise docker image for self host setup
    
    * adding api access to traces endpoint
    
    * clean up
    
    * refactor
    
    * feat: add clickhouse db create on app start (#79)
    
    ---------
    
    Co-authored-by: Karthik Kalyanaraman <105607645+karthikscale3@users.noreply.github.com>
    Co-authored-by: dylan <dylan@scale3labs.com>
    Co-authored-by: dylanzuber-scale3 <116033320+dylanzuber-scale3@users.noreply.github.com>

--- app/api/traces/route.ts
+++ app/api/traces/route.ts
@@ -1,5 +1,7 @@
 import { authOptions } from "@/lib/auth/options";
+import prisma from "@/lib/prisma";
 import { TraceService } from "@/lib/services/trace_service";
+import { hashApiKey } from "@/lib/utils";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import { NextRequest, NextResponse } from "next/server";
@@ -7,12 +9,34 @@ import { NextRequest, NextResponse } from "next/server";
 export async function POST(req: NextRequest) {
   try {
     const session = await getServerSession(authOptions);
-
+    const apiKey = req.headers.get("x-api-key");
+    const { page, pageSize, projectId, filters } = await req.json();
     if (!session || !session.user) {
-      redirect("/login");
+      if (apiKey) {
+        const project = await prisma.project.findFirst({
+          where: {
+            id: projectId,
+          },
+        });
+
+        if (!project) {
+          return NextResponse.json(
+            { error: "No projects found" },
+            { status: 404 }
+          );
+        }
+
+        if (apiKey && project.apiKeyHash !== hashApiKey(apiKey)) {
+          return NextResponse.json(
+            { error: "Unauthorized. Invalid API key" },
+            { status: 401 }
+          );
+        }
+      } else {
+        redirect("/login");
+      }
     }
 
-    const { page, pageSize, projectId, filters } = await req.json();
     const traceService = new TraceService();
     const traces = await traceService.GetTracesInProjectPaginated(
       projectId,
