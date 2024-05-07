506d0b399cce564595d059b07b9bfde10e8f469d
Karthik Kalyanaraman <105607645+karthikscale3@users.noreply.github.com>
2024-05-07 11:25:08 -0700

    Release 1.3.1 (#87)
    * Pagination bug
    
    * Bug fix
    
    * chore: add docker cmd
    
    * Compatibility fixes for SDK version 2.0.0 (#69)
    
    * Pagination bug
    
    * Bug fix
    
    * Fix for schema changes
    
    * Render tool calling
    
    * Support for Langgraph, Qdrant & Groq  (#73)
    
    * Pagination bug
    
    * Bug fix
    
    * Add langgraph support
    
    * QDrant support
    
    * Add Groq support
    
    * update README
    
    * update README
    
    * feat: optimise docker image for self host setup
    
    * adding api access to traces endpoint
    
    * clean up
    
    * refactor
    
    * feat: add clickhouse db create on app start (#79)
    
    * docs: add railway deploy, fix sdk badges (#81)
    
    * Playground and Prompt Management (#83)
    
    * Pagination bug
    
    * Bug fix
    
    * Playground - basic implementation
    
    * Playground - streaming and nonstreaming
    
    * Move playground inside project
    
    * API key flow
    
    * Api key
    
    * Playground refactor
    
    * Add chat hookup
    
    * anthropic streaming support
    
    * Bug fixes to openai playground
    
    * Anthropic bugfixes
    
    * Anthropic bugfix
    
    * Cohere first iteration
    
    * Cohere role fixes
    
    * Cohere api fix
    
    * Parallel running
    
    * Playground cost calculation non streaming
    
    * playground - streaming token calculation
    
    * latency and cost
    
    * Support for Groq
    
    * Add model name
    
    * Prompt management views
    
    * Remove current promptset flow
    
    * Prompt management API hooks
    
    * Prompt registry final
    
    * Playground bugfixes
    
    * Bug fix playground
    
    * Rearrange project nav
    
    * Fix playground
    
    * Fix prompts
    
    * Bugfixes
    
    * Minor fix
    
    * Prompt versioning bugfix
    
    * Bugfix
    
    * fix: clickhouse table find queries (#82)
    
    * Fix to surface multiple LLM requests inside LLM View (#84)
    
    * Pagination bug
    
    * Bug fix
    
    * Fix for surfacing multiple LLM requests in LLMView
    
    * Minor bugfixes (#86)
    
    * Pagination bug
    
    * Bug fix
    
    * Bugfixes
    
    ---------
    
    Co-authored-by: Darshit Suratwala <darshit@scale3labs.com>
    Co-authored-by: darshit-s3 <119623510+darshit-s3@users.noreply.github.com>
    Co-authored-by: dylan <dylan@scale3labs.com>
    Co-authored-by: dylanzuber-scale3 <116033320+dylanzuber-scale3@users.noreply.github.com>

--- app/(protected)/project/[project_id]/playground/page.tsx
+++ app/(protected)/project/[project_id]/playground/page.tsx
@@ -75,7 +75,7 @@ export default function Page() {
   return (
     <div className="px-12 py-6 flex flex-col gap-8">
       <span className="text-sm font-semibold">
-        Note: Don't forget to add your LLM provider API keys in the{" "}
+        Note: Dont forget to add your LLM provider API keys in the{" "}
         <Link href="/settings/keys" className="underline text-blue-400">
           settings page.
         </Link>
