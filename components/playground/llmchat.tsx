7b31d32bbbe66de1fb8235dcf499a5e999a33710
dylan <dylan@scale3labs.com>
2024-06-04 14:21:48 -0400

    init import conversation dialog

--- components/playground/llmchat.tsx
+++ components/playground/llmchat.tsx
@@ -33,6 +33,7 @@ import {
   perplexityHandler,
 } from "./chat-handlers";
 import { Message } from "./common";
+import ImportConversationDialog from "./import-conversation-dialog";
 import {
   AnthropicSettingsSheet,
   CohereSettingsSheet,
@@ -40,10 +41,6 @@ import {
   OpenAISettingsSheet,
   PerplexitySettingsSheet,
 } from "./settings-sheet";
-
-function identity<T>(value: T): T {
-  return value;
-}
 
 export default function LLMChat({
   llm,
@@ -60,6 +57,8 @@ export default function LLMChat({
   const [apiKey, setApiKey] = useState<string | null>(null);
   const [cost, setCost] = useState("");
   const [latency, setLatency] = useState("");
+  const [importConversationDialogOpen, setImportConversationDialogOpen] =
+    useState(false);
 
   useEffect(() => {
     const vendor = LLM_VENDOR_APIS.find(
@@ -111,8 +110,26 @@ export default function LLMChat({
           />
         ))}
       </div>
-      {/* <div className="w-full flex items-center px-2 justify-center"> */}
-      <div className="w-full flex items-center px-2 justify-center space-x-2 mt-2">
+      {llm.settings.messages.length === 0 && (
+        <div className="absolute inset-0 flex items-center justify-center z-10">
+          <Button
+            type="button"
+            size="lg"
+            variant="default"
+            onClick={() => {
+              setImportConversationDialogOpen(true);
+            }}
+          >
+            Import Conversation
+          </Button>
+          <ImportConversationDialog
+            openDialog={importConversationDialogOpen}
+            setOpenDialog={setImportConversationDialogOpen}
+            // passedPrompt={message.content}
+          />
+        </div>
+      )}
+      <div className="w-full flex items-center justify-center">
         <Button
           type="button"
           variant={llm.settings.messages.length === 0 ? "default" : "secondary"}
@@ -157,403 +174,6 @@ export default function LLMChat({
         >
           <PlusCircleIcon className="mr-2 h-4 w-4" />
           Add message
-        </Button>
-        <Button
-          size={"sm"}
-          variant={llm.settings.messages.length === 0 ? "secondary" : "default"}
-          className="absolute bottom-4 left-4"
-          disabled={busy || llm.settings.messages.length === 0}
-          onClick={async () => {
-            setBusy(true);
-            try {
-              // Calculate latency
-              const startTime = performance.now();
-              let response: any;
-              if (localLLM.vendor === "openai") {
-                response = await openAIHandler(
-                  localLLM as OpenAIChatInterface,
-                  apiKey || ""
-                );
-              } else if (localLLM.vendor === "anthropic") {
-                response = await anthropicHandler(
-                  localLLM as AnthropicChatInterface,
-                  apiKey || ""
-                );
-              } else if (localLLM.vendor === "cohere") {
-                response = await cohereHandler(
-                  localLLM as CohereChatInterface,
-                  apiKey || ""
-                );
-              } else if (localLLM.vendor === "groq") {
-                response = await groqHandler(
-                  localLLM as GroqChatInterface,
-                  apiKey || ""
-                );
-              } else if (localLLM.vendor === "perplexity") {
-                response = await perplexityHandler(
-                  localLLM as PerplexityChatInterface,
-                  apiKey || ""
-                );
-              }
-
-              if (localLLM.settings.stream === true) {
-                const reader = response?.body?.getReader();
-                const decoder = new TextDecoder("utf-8");
-
-                // Read the stream
-                let receivedLength = 0;
-                let chunks = [];
-                let chunkTexts = [];
-                while (true) {
-                  const { done, value } = (await reader?.read()) as {
-                    done: boolean;
-                    value: Uint8Array;
-                  };
-
-                  if (done) {
-                    break;
-                  }
-
-                  chunks.push(value);
-                  receivedLength += value.length;
-
-                  // Decode chunk as text
-                  const chunkText = decoder.decode(value, { stream: true });
-                  chunkTexts.push(chunkText);
-                  setLocalLLM({
-                    ...localLLM,
-                    settings: {
-                      ...localLLM.settings,
-                      messages: [
-                        ...localLLM.settings.messages,
-                        {
-                          id: uuidv4(),
-                          role:
-                            localLLM.vendor === "cohere"
-                              ? CohereAIRole.chatbot
-                              : OpenAIRole.assistant,
-                          content: chunkTexts.join(""),
-                        },
-                      ],
-                    },
-                  });
-                  setLLM({
-                    ...llm,
-                    settings: {
-                      ...llm.settings,
-                      messages: [
-                        ...llm.settings.messages,
-                        {
-                          id: uuidv4(),
-                          role:
-                            llm.vendor === "cohere"
-                              ? CohereAIRole.chatbot
-                              : OpenAIRole.assistant,
-                          content: chunkTexts.join(""),
-                        },
-                      ],
-                    },
-                  });
-                }
-
-                // Concatenate all chunks to create a final data string
-                const chunksAll = new Uint8Array(receivedLength);
-                let position = 0;
-                for (let chunk of chunks) {
-                  chunksAll.set(chunk, position);
-                  position += chunk.length;
-                }
-
-                // Decode all chunks into text
-                const result = decoder.decode(chunksAll);
-
-                // ugly hack to check if the response is an error
-                if (result.includes('{"error":"401')) {
-                  toast.error(
-                    <div className="flex flex-col gap-2 items-start">
-                      <p>
-                        API key is invalid or not found. Update your API key
-                      </p>
-                      <div className="w-fit">
-                        <Link
-                          href={"/settings/keys"}
-                          className="underline text-sm font-semibold flex items-center"
-                        >
-                          Update API key
-                          <ArrowTopRightIcon className="h-4 w-4 ml-1" />
-                        </Link>
-                      </div>
-                    </div>
-                  );
-                  const endTime = performance.now();
-                  setLatency((endTime - startTime).toFixed(2));
-                  return;
-                }
-
-                setLLM({
-                  ...llm,
-                  settings: {
-                    ...llm.settings,
-                    messages: [
-                      ...llm.settings.messages,
-                      {
-                        id: uuidv4(),
-                        role:
-                          llm.vendor === "cohere"
-                            ? CohereAIRole.chatbot
-                            : OpenAIRole.assistant,
-                        content: result,
-                      },
-                    ],
-                  },
-                });
-                setLocalLLM({
-                  ...localLLM,
-                  settings: {
-                    ...localLLM.settings,
-                    messages: [
-                      ...localLLM.settings.messages,
-                      {
-                        id: uuidv4(),
-                        role:
-                          localLLM.vendor === "cohere"
-                            ? CohereAIRole.chatbot
-                            : OpenAIRole.assistant,
-                        content: result,
-                      },
-                    ],
-                  },
-                });
-
-                const endTime = performance.now();
-                setLatency((endTime - startTime).toFixed(2));
-
-                // cost calculation
-                const inputContent =
-                  localLLM.settings.messages[
-                    localLLM.settings.messages.length - 1
-                  ]?.content ?? "";
-                const inputTokens = calculateTokens(
-                  inputContent,
-                  localLLM.settings.model
-                );
-                const outputTokens = calculateTokens(
-                  result,
-                  localLLM.settings.model
-                );
-                const vendor = localLLM.vendor;
-                const model = localLLM.settings.model;
-                const calculatedCost = calculatePriceFromUsage(vendor, model, {
-                  input_tokens: inputTokens,
-                  output_tokens: outputTokens,
-                });
-                const totalCost =
-                  calculatedCost.total.toFixed(6) !== "0.000000"
-                    ? `\$${calculatedCost.total.toFixed(6)}`
-                    : "";
-                setCost(totalCost);
-              } else {
-                const data = await response.json();
-                if (data.error) {
-                  let message =
-                    data.error ?? "An error occurred. Please try again.";
-                  if (data.status === 401) {
-                    toast.error(
-                      <div className="flex flex-col gap-2 items-start">
-                        <p>
-                          API key is invalid or not found. Update your API key
-                        </p>
-                        <div className="w-fit">
-                          <Link
-                            href={"/settings/keys"}
-                            className="underline text-sm font-semibold flex items-center"
-                          >
-                            Update API key
-                            <ArrowTopRightIcon className="h-4 w-4 ml-1" />
-                          </Link>
-                        </div>
-                      </div>
-                    );
-                  } else {
-                    toast.error(message);
-                  }
-                  return;
-                }
-                const endTime = performance.now();
-                setLatency((endTime - startTime).toFixed(2));
-                let message = "";
-                if (
-                  localLLM.vendor === "openai" ||
-                  localLLM.vendor === "groq"
-                ) {
-                  if (data?.choices?.length > 0) {
-                    if (data.choices[0]?.message?.content) {
-                      message = data.choices[0].message.content;
-                    } else if (
-                      data.choices[0]?.message?.tool_calls.length > 0
-                    ) {
-                      message = JSON.stringify(
-                        data.choices[0].message.tool_calls
-                      );
-                    }
-                  }
-                  if (data?.usage) {
-                    const inputTokens = data.usage.prompt_tokens;
-                    const outputTokens = data.usage.completion_tokens;
-                    const vendor = localLLM.vendor;
-                    const model = localLLM.settings.model;
-                    const calculatedCost = calculatePriceFromUsage(
-                      vendor,
-                      model,
-                      {
-                        input_tokens: inputTokens,
-                        output_tokens: outputTokens,
-                      }
-                    );
-                    const totalCost =
-                      calculatedCost.total.toFixed(6) !== "0.000000"
-                        ? `\$${calculatedCost.total.toFixed(6)}`
-                        : "";
-                    setCost(totalCost);
-                  }
-                } else if (localLLM.vendor === "anthropic") {
-                  if (data?.content?.length > 0) {
-                    if (data.content[0].type === "text") {
-                      message = data.content[0].text;
-                    } else {
-                      message = JSON.stringify(data.content[0].text);
-                    }
-                  }
-                  if (data?.usage) {
-                    const inputTokens = data.usage.input_tokens;
-                    const outputTokens = data.usage.output_tokens;
-                    const vendor = localLLM.vendor;
-                    const model = localLLM.settings.model;
-                    const calculatedCost = calculatePriceFromUsage(
-                      vendor,
-                      model,
-                      {
-                        input_tokens: inputTokens,
-                        output_tokens: outputTokens,
-                      }
-                    );
-                    const totalCost =
-                      calculatedCost.total.toFixed(6) !== "0.000000"
-                        ? `\$${calculatedCost.total.toFixed(6)}`
-                        : "";
-                    setCost(totalCost);
-                  }
-                } else if (localLLM.vendor === "cohere") {
-                  if (data?.text) {
-                    if (typeof data.text === "object") {
-                      message = JSON.stringify(data.text);
-                    } else {
-                      message = data?.text;
-                    }
-                  }
-                  if (data.meta?.billedUnits) {
-                    const inputTokens = data.meta?.billedUnits?.inputTokens;
-                    const outputTokens = data.meta?.billedUnits?.outputTokens;
-                    const vendor = localLLM.vendor;
-                    const model = localLLM.settings.model;
-                    const calculatedCost = calculatePriceFromUsage(
-                      vendor,
-                      model,
-                      {
-                        input_tokens: inputTokens,
-                        output_tokens: outputTokens,
-                      }
-                    );
-                    const totalCost =
-                      calculatedCost.total.toFixed(6) !== "0.000000"
-                        ? `\$${calculatedCost.total.toFixed(6)}`
-                        : "";
-                    setCost(totalCost);
-                  }
-                } else if (localLLM.vendor === "perplexity") {
-                  if (data?.choices?.length > 0) {
-                    if (data.choices[0]?.message?.content) {
-                      message = data.choices[0].message.content;
-                    }
-                  }
-                  if (data?.usage) {
-                    const inputTokens = data.usage.prompt_tokens;
-                    const outputTokens = data.usage.completion_tokens;
-                    const vendor = localLLM.vendor;
-                    const model = localLLM.settings.model;
-                    const calculatedCost = calculatePriceFromUsage(
-                      vendor,
-                      model,
-                      {
-                        input_tokens: inputTokens,
-                        output_tokens: outputTokens,
-                      }
-                    );
-                    const totalCost =
-                      calculatedCost.total.toFixed(6) !== "0.000000"
-                        ? `\$${calculatedCost.total.toFixed(6)}`
-                        : "";
-                    setCost(totalCost);
-                  }
-                }
-                setLLM({
-                  ...llm,
-                  settings: {
-                    ...llm.settings,
-                    messages: [
-                      ...llm.settings.messages,
-                      {
-                        id: uuidv4(),
-                        role:
-                          llm.vendor === "cohere"
-                            ? CohereAIRole.chatbot
-                            : OpenAIRole.assistant,
-                        content: message,
-                      },
-                    ],
-                  },
-                });
-                setLocalLLM({
-                  ...localLLM,
-                  settings: {
-                    ...localLLM.settings,
-                    messages: [
-                      ...localLLM.settings.messages,
-                      {
-                        id: uuidv4(),
-                        role:
-                          localLLM.vendor === "cohere"
-                            ? CohereAIRole.chatbot
-                            : OpenAIRole.assistant,
-                        content: message,
-                      },
-                    ],
-                  },
-                });
-              }
-            } catch (error) {
-              toast.error("An error occurred. Please try again.");
-            } finally {
-              setBusy(false);
-            }
-          }}
-        >
-          Submit
-          {busy && (
-            <Image
-              src={
-                theme === "dark" ? "/spinner-dark.svg" : "/spinner-light.svg"
-              }
-              alt="Spinner"
-              width={15}
-              height={15}
-              className="ml-2"
-            />
-          )}
-          {!busy && <LucideChevronRight className="ml-2 h-4 w-4" />}
-        </Button>
-        <Button type="button" className="mt-2 px-w" size={"sm"}>
-          Import conversation
         </Button>
       </div>
       <div className="absolute -top-6 -left-3">
@@ -612,7 +232,7 @@ export default function LLMChat({
       >
         <MinusIcon className="h-4 w-4" />
       </Button>
-      {/* <Button
+      <Button
         size={"sm"}
         variant={llm.settings.messages.length === 0 ? "secondary" : "default"}
         className="absolute bottom-4 left-4"
@@ -996,7 +616,7 @@ export default function LLMChat({
           />
         )}
         {!busy && <LucideChevronRight className="ml-2 h-4 w-4" />}
-      </Button> */}
+      </Button>
       {(cost || latency) && (
         <div className="absolute bottom-2 right-4 flex flex-col gap-2 bg-primary-foreground rounded-md p-2 w-[115px]">
           <p className="text-xs">{`Cost: ${cost}`}</p>
