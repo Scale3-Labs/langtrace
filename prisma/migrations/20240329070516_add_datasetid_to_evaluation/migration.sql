1b99bc9154600f3abd5edac3c4e0f82328256474
darshit-s3 <119623510+darshit-s3@users.noreply.github.com>
2024-07-26 10:49:26 -0700

    refactor migrations flow for langtrace (#211)
    * feat: refactor migrations process
    
    * chore: fail script automatically during start
    
    * Update prisma/migrations/0_init/migration.sql
    
    * chore: remove additional columns
    
    * feat: handle init failed migrations
    
    ---------
    
    Co-authored-by: Karthik Kalyanaraman <karthik@scale3labs.com>

--- prisma/migrations/20240329070516_add_datasetid_to_evaluation/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `datasetId` on the `Evaluation` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Evaluation" DROP COLUMN "datasetId",
-ADD COLUMN     "addedToDataset" BOOLEAN DEFAULT false;
